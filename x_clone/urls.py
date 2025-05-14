31cc06580e341b32ca95ba1f4cd3c1eac3d5cd62
yuta-yamaura <shk0fate@gmail.com>
2025-05-14 17:16:35 +0900

    ログイン機能の土台を作成

--- x_clone/urls.py
+++ x_clone/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
     path('users/register/', views.registerUser ,name='register'), # ユーザー作成
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
