9217f02a7c5e4aa907f231efbf386e056d414177
yuta-yamaura <shk0fate@gmail.com>
2025-05-26 13:49:14 +0900

    ログイン認証をviews.pyからserializer.pyに移行

--- x_clone/urls.py
+++ x_clone/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+from .serializers import MyTokenObtainPairView
 
 urlpatterns = [
     path('users/register/', views.registerUser ,name='register'), # ユーザー作成
-    path('users/login/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- x_clone/views.py
+++ x_clone/views.py
@@ -3,22 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from .serializers import UserSerializerWithToken
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs: dict[str, any]) -> dict[str, str]:
-        data = super().validate(attrs)
-
-        serializer = UserSerializerWithToken(self.user).data
-        for k, v in serializer.items():
-            data[k] = v
-
-        return data
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['POST'])
 def registerUser(request):
