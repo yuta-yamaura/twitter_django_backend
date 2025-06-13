2a53eff28ca77e6f5b625d6dd3ccc359de5b8473
yuta-yamaura <shk0fate@gmail.com>
2025-06-13 10:45:03 +0900

    Comment作成テストを追加

--- comments/tests.py
+++ comments/tests.py
@@ -7,6 +7,45 @@ from django.urls import reverse
 from rest_framework import status
 
 # Create your tests here.
+class CommentCreateViewTest(APITestCase):
+    # テストユーザーの作成
+    @classmethod
+    def setUpTestData(cls):
+        # Original User
+        cls.user = User.objects.create_user(
+            username='origin',
+            email='origin@gmail.com',
+            telephone_number='080-1234-5678',
+            password='password'
+            )
+        
+        cls.tweet = Tweet.objects.create(
+            user=cls.user,
+            content='テスト用ツイート'
+        )
+    
+    def setUp(self):
+        # 標準のリクエストメソッド(put,delete,patch,etc...)が使えるようセットアップ
+        self.client = APIClient()
+        # リクエストを強制的に認証
+        self.client.force_authenticate(user=self.user)
+    
+    def test_comment_create_success(self):
+        """Comment作成のテスト"""
+        url = reverse('tweet_comments_list_create', kwargs={'tweet_id': self.tweet.pk})
+        data = {'comment': 'comment create test'}
+        response = self.client.post(url, data, format='json')
+        
+        # レスポンスから作成されたコメントのidを取得
+        comment_id = response.data['id']
+        comment = Comment.objects.get(pk=comment_id)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.filter(pk=comment.pk).count(), 1)
+        self.assertEqual(comment.comment, 'comment create test')
+        self.assertEqual(comment.user, self.user)
+        self.assertEqual(comment.tweet, self.tweet)
+
 class CommentDeleteViewTest(APITestCase):
     # テストユーザーの作成
     @classmethod
