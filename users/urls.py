d14edf65277c0a1831a35afcff7655004e9aaf86
yuta-yamaura <shk0fate@gmail.com>
2025-07-05 15:00:52 +0900

    ホーム画面にユーザーのアイコンを表示

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from .serializers import MyTokenObtainPairView
 urlpatterns = [
     path('register/', views.registerUser, name='register'), # ユーザー作成
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('info/', views.UserInfo.as_view(), name='info'), # ホーム画面のユーザー情報取得
     path('profile/<int:pk>/', views.UserUpdateView.as_view(), name='user-profile'),
     path('delete/', views.UserDeleteView.as_view(), name='user_delete'),
 ]
